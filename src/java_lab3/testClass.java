39e947e3578034a3b1354922b28134d6fc49533a
AnjanaBiju <59994175+AnjanaBiju@users.noreply.github.com>
2020-03-09 15:27:17 +0530

    test class
    First attempt

---
+++ src/java_lab3/testClass.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_lab3;
+
+/**
+ *
+ * @author MCALAB01128
+ */
+public class testClass {
+    public static void main(String args[]){
+        System.out.println("Hai");
+    }
+}
